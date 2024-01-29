12d04b1e35e2b541a04d39504acaf7226c6e3919
santino majur <demalong22@gmail.com>
2024-01-29 16:30:46 +0200

    Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-// import airbnbLogo from '../images/airbnb-logo.png'
+import airbnbLogo from '../images/airbnb-logo.png'
 
-// export default function Navbar() {
-//     return (
-//         <div className="">
-//             <img src={airbnbLogo} alt="airbnb logo" />
-//         </div>
-//     )
-// }
+export default function Navbar() {
+    return (
+        <div className="py-6 px-8 md:px-1 h-[70px] shadow-md text-center ">
+            <div className='md:max-w-xl lg:max-w-3xl mx-auto'>
+                <img src={airbnbLogo} alt="airbnb logo" className='w-[82px] h-[25px]'/>
+            </div>
+            
+        </div>
+    )
+}
